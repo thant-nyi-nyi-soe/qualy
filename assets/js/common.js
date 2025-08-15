fa7ff1e18a806996a0eeb5898e2ec59a4eee2fc6
thant-nyi-nyi-soe <thantnyinyisoe.qualymm@gmail.com>
2025-08-15 16:14:04 +0630

    three parts left getting nowhere

--- assets/js/common.js
+++ assets/js/common.js
@@ -16,13 +16,39 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       serviceContents.forEach((content) => {
-        if(content.classList.contains(`content-for-${tabId}`)) {
+        if (content.classList.contains(`content-for-${tabId}`)) {
           content.classList.add("content-active");
         } else {
           content.classList.remove("content-active");
-        };
-      })
+        }
+      });
     });
+  });
+
+  window.addEventListener("scroll", () => {
+    const toTop = document.querySelector(".back-to-top-btn");
+    if (
+      document.body.scrollTop > 200 ||
+      document.documentElement.scrollTop > 200
+    ) {
+      toTop.classList.remove("no-back-to-top");
+    } else {
+      toTop.classList.add("no-back-to-top");
+    }
+  });
+
+  /* MAKE BACK TO TOP BUTTON STICK TO THE TOP OF THE FOOTER */
+  window.addEventListener("scroll", () => {
+    const toTop = document.querySelector(".back-to-top-btn");
+    const footer = document.querySelector(".footer-wrapper");
+    const footerTop = footer.getBoundingClientRect().top;
+    const winHeight = window.innerHeight;
+
+    if (footerTop < winHeight) {
+      toTop.classList.add("above-footer");
+    } else {
+      toTop.classList.remove("above-footer");
+    }
   });
 });
 
@@ -36,8 +62,35 @@ $(function () {
   $(".top-slider").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    infinite: true,
     arrows: false,
     dots: true,
+  });
+
+  $(".discount-cards-slider").slick({
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: "60px",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          dots: true,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          dots: true,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 });
